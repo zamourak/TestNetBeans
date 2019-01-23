8dabb4337d7aec87826fb5b77df4d6060ba3a56a
zamourak <zamourak@DESKTOP-J2U4EBB>
2019-01-23 17:57:12 +0100

    Grosse couille

---
+++ src/testgit/TestGit.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgit;
+
+/**
+ *
+ * @author zamourak
+ */
+public class TestGit {
+    private int Macouille;
+    public TestGit(int Macouille){
+        this.Macouille = Macouille;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TestGit mc = new TestGit(2);
+        System.out.println(mc);
+        // TODO code application logic here
+    }
+    
+}
